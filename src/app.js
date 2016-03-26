588e69db18422b853b947ac726adf232ce605ca4
Seth Bergman <seth.atxwebs@gmail.com>
2016-03-26 12:49:26 -0500

    added Procfile

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ var app = express();
 
 app.use('/', express.static('public'));
 
-app.listen(3000, function(){
-  console.log("The server is running on port 3000!");
-});
+var server = app.listen(5000, function () {
+  console.log('Server running at 0.0.0.0:' + server.address().port)
+})
