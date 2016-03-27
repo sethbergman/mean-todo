15e3dadbf879236459709e08e472ccef7e2f01f5
Seth Bergman <seth.atxwebs@gmail.com>
2016-03-27 05:57:37 -0500

    built fully functional api with MEAN stack

--- public/README.md
+++
@@ -1,8 +0,0 @@
-# angular-basics
-
-# set-up
-```
-git clone <this-repo>
-cd <this-repo>
-http-server -p 3000
-# visit localhost:3000 to see the todoListApp
--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var express = require('express');
+var parser = require('body-parser');
+var router = require('./api');
 
 var app = express();
 
+require('./database');
+require('./seed');
+
 app.use('/', express.static('public'));
+app.use(parser.json());
+
+app.use('/api', router);
 
 var server = app.listen(5000, function () {
   console.log('Server running at 0.0.0.0:' + server.address().port)
