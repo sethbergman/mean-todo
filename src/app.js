ab458dcd0f774e807191d7058969a7f2dbb3b279
Seth Bergman <seth.atxwebs@gmail.com>
2016-04-16 16:30:06 -0500

    added favicon

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var express = require('express');
+var favicon = require('serve-favicon');
 
 var app = express();
 
+app.use(favicon(__dirname + '/public/images/favicon.png'));
 app.use('/', express.static('public'));
 
-var server = app.listen(5000, function () {
-  console.log('Server running at 0.0.0.0:' + server.address().port)
+var server = app.listen(process.env.PORT | 5000, function () {
+  console.log('Server running at http://0.0.0.0:' + server.address().port)
 })
